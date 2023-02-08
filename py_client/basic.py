607c1450be7421c8822b1aad3ba0a1bb057d014d
TIENTCHEUisrael <israel01tientcheu@gmail.com>
2023-02-08 05:38:05 +0100

    django test

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,13 +1,16 @@
 import requests
 
-endpoint="https://httpbin.org/status/200/"
-endpoint="https://httpbin.org/anything"
+#endpoint="https://httpbin.org/status/200/"
+#endpoint="https://httpbin.org/anything"
+endpoint="http://localhost:8000/"  #http://127.0.0.1:8000/
 
-get_response=requests.get(endpoint) #HTTP request
+get_response=requests.get(endpoint,json={"query":"Hello world"}) #HTTP request
 print(get_response.text) #print the raw text response
+print(get_response.status_code)
 
 #HTTP Request -> HTML
 #Rest Api HTTP Request -> JSON
 # JavaScript Object Notation ~ Python Dictionnary
 
-print(get_response.json)
+#print(get_response.json()) 
+#print(get_response.status_code)

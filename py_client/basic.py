82a68f5adc96f7981096172d0b3879c1269e7126
TIENTCHEUisrael <israel01tientcheu@gmail.com>
2023-02-08 05:54:30 +0100

    creation of api and test into the terminal run python py_client/basic.py after python manage.py runserver 8000

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,8 @@ import requests
 
 #endpoint="https://httpbin.org/status/200/"
 #endpoint="https://httpbin.org/anything"
-endpoint="http://localhost:8000/"  #http://127.0.0.1:8000/
+#endpoint="http://localhost:8000/"  #http://127.0.0.1:8000/
+endpoint="http://localhost:8000/api/"
 
 get_response=requests.get(endpoint,json={"query":"Hello world"}) #HTTP request
 print(get_response.text) #print the raw text response
@@ -12,5 +13,5 @@ print(get_response.status_code)
 #Rest Api HTTP Request -> JSON
 # JavaScript Object Notation ~ Python Dictionnary
 
-#print(get_response.json()) 
+print(get_response.json()['message']) 
 #print(get_response.status_code)

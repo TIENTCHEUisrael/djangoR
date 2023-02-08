42babd0c6ffff0d1d4395fbca721e7c292ff1ab1
TIENTCHEUisrael <israel01tientcheu@gmail.com>
2023-02-08 20:58:44 +0100

    Present affichage of json result

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -1,5 +1,22 @@
 #from django.shortcuts import render
 from django.http import JsonResponse
-
+import json
 def api_home(request,*args,**kwargs):
-    return JsonResponse({"message":"Hi this is your django api response"})
+    # request -> HttpRequest -> Django
+    # print(dir(request))
+    # request.body
+    
+    print(request.GET) #url query params
+    print(request.POST)
+    body=request.body #byte string of JSON data
+    print(body) #present
+    data={}
+    try:
+        data=json.loads(body) # String of data -> Python Dict
+    except:
+        pass
+    print(data)
+    data['params']=dict(request.GET)
+    data['headers']=dict(request.headers)
+    data['content_type']=request.content_type
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,7 +5,7 @@ import requests
 #endpoint="http://localhost:8000/"  #http://127.0.0.1:8000/
 endpoint="http://localhost:8000/api/"
 
-get_response=requests.get(endpoint,json={"query":"Hello world"}) #HTTP request
+get_response=requests.get(endpoint,params={"abc":123},json={"query":"Hello world"}) #HTTP request
 print(get_response.text) #print the raw text response
 print(get_response.status_code)
 
@@ -13,5 +13,5 @@ print(get_response.status_code)
 #Rest Api HTTP Request -> JSON
 # JavaScript Object Notation ~ Python Dictionnary
 
-print(get_response.json()['message']) 
+#print(get_response.json()['message']) 
 #print(get_response.status_code)

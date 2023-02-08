79e2a100d8e799c1629b1c2961bd09852ef38927
TIENTCHEUisrael <israel01tientcheu@gmail.com>
2023-02-08 21:26:52 +0100

    Add product application and models for  using

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -1,6 +1,8 @@
 #from django.shortcuts import render
 from django.http import JsonResponse
 import json
+
+
 def api_home(request,*args,**kwargs):
     # request -> HttpRequest -> Django
     # print(dir(request))
